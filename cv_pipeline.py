227e0f6aa10b58a526abd86b3ac0dd265ac9702b
KingChad127 <abhinavc@cs.utexas.edu>
2022-05-12 00:38:51 -0500

    changes

--- cv_pipeline.py
+++ cv_pipeline.py
@@ -17,20 +17,31 @@ results = model.detect([img_arr], verbose=1)
 r = results[0]
 
 label_class = 1
+button_class = 2
 masks = r["masks"]
 class_ids = r["class_ids"]
 
 label_masks = masks[:, :, np.where(class_ids == label_class)[0]]
+button_masks = masks[:, :, np.where(class_ids == button_class)[0]]
 labels = []
+buttons = []
 for i in range(label_masks.shape[2]):
-    temp = skimage.io.imread('./elevator_panels/val/0.jpg')
-    for j in range(temp.shape[2]):
-        temp[:, :, j] = temp[:, :, j] * label_masks[:, :, i]
-    temp = cv2.resize(temp, (1024, 1024))
-    labels.append(temp)
+    label_tmp = skimage.io.imread('./elevator_panels/val/0.jpg')
+    button_tmp = skimage.io.imread('./elevator_panels/val/0.jpg')
+    for j in range(label_tmp.shape[2]):
+        label_tmp[:, :, j] = label_tmp[:, :, j] * label_masks[:, :, i]
+        button_tmp[:, :, j] = button_tmp[:, :, j] * button_masks[:, :, i]
+    label_tmp = cv2.resize(label_tmp, (1024, 1024))
+    button_tmp = cv2.resize(button_tmp, (1024, 1024))
+    labels.append(label_tmp)
+    buttons.append(button_tmp)
 
-for label in labels:
-    cv2.imshow("first label", labels[0].astype(np.uint8))
+for i, label in enumerate(labels):
+    cv2.imshow("label " + str(i), label.astype(np.uint8))
+    cv2.waitKey(0)
+
+for i, button in enumerate(buttons):
+    cv2.imshow("label " + str(i), button.astype(np.uint8))
     cv2.waitKey(0)
 
 cv2.destroyAllWindows()
@@ -47,3 +58,4 @@ cv2.destroyAllWindows()
 
 
 # visualize.display_top_masks(img_arr, r["masks"], r["class_ids"], class_names)
+ 
